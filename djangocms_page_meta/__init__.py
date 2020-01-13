51c5bb9bec05fb766bdadd7b9d39bb8a2743e615
Martin Kjellberg <martin@martinkjellberg.com>
2020-01-13 15:28:40 +0100

    Set new version

--- djangocms_page_meta/__init__.py
+++ djangocms_page_meta/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = '0.10.0'
-__author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
+__version__ = '0.10.1'
+__author__ = 'Martin Kjellberg <martin@martinkjellberg.com>'
+# __author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
 
 default_app_config = 'djangocms_page_meta.apps.PageMetaConfig'
